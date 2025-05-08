3b1f1520c33b2af0b9d45d7c589f351063d8892b
Bennor McCarthy <bennor.mccarthy@vercel.com>
2025-05-08 14:28:36 +1000

    fix lint errors

--- src/utils/tracing.ts
+++ src/utils/tracing.ts
@@ -1,6 +1,6 @@
 import type { Span, Tracer } from '@opentelemetry/api';
 
-export async function withSpan<T extends Promise<any> = Promise<void>>(
+export async function withSpan<T extends Promise<unknown> = Promise<void>>(
   tracer: Tracer,
   operationName: string,
   operation: (span: Span) => T
